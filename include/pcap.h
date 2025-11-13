d30c85a5eb0f2e3eeb565f692db135178a7a283e
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-11-12 23:29:58 -0500

    Added header guard for PCAP_H

--- include/pcap.h
+++ include/pcap.h
@@ -1,3 +1,8 @@
+/* Header Guard(s) */
+#ifndef PCAP_H
+#define PCAP_H
+
+/* Include Statements */
 #include <stdint.h>
 
 typedef struct pcap_global_header {
@@ -8,14 +13,16 @@ typedef struct pcap_global_header {
     uint32_t sigfigs;        /* accuracy of timestamps */
     uint32_t snaplen;        /* max length of captured packets, in octets */
     uint32_t network;        /* data link type as defined by https://www.tcpdump.org/linktypes.html*/
-} pcap_global_header;
+} pcap_global_header_t;
 
 typedef struct pcap_record_header {
     uint32_t ts_sec;         /* timestamp seconds */
     uint32_t ts_usec;        /* timestamp microseconds */
     uint32_t incl_len;       /* number of octets of packet saved in file */
     uint32_t orig_len;       /* actual length of packet */
-} pcap_record_header;
+} pcap_record_header_t;
 
 
 int process_pcap_file(const char* filepath);
+
+#endif
