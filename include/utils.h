d6d0f89213f21d7cac21725d90a0ec0839383903
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-11-14 14:52:06 -0500

    Added signatures for swapping to 16-bit and 32-bit values

---
+++ include/utils.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#ifndef UTILS_H
+#define UTILS_H
+/* Function to swap a 16-bit value */
+uint16_t swap_uint16(uint16_t val);
+/* Function to swap a 32-bit value */
+uint32_t swap_uint32(uint32_t val);
+#endif /* UTILS_H */
