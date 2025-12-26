2fd59dd3e99dd70c5eb0d9d0e1641c72f2f909be
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-12-26 16:08:18 -0500

    Added macros to handle byte swapping of data based on file context

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,28 @@
 #include <stdint.h>
 #ifndef UTILS_H
 #define UTILS_H
-/* Function to swap a 16-bit value */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PCAP_MAGIC_NUMBER 0xa1b2c3d4
+#define PCAP_SWAPPED_MAGIC_NUMBER 0xd4c3b2a1
+
+/* Macros to handle byte swapping based on file context */
+#define BYTES_TO_UINT16(val) (file_ctx->swap_bytes ? swap_uint16(val) : (val))
+#define BYTES_TO_UINT32(val) (file_ctx->swap_bytes ? swap_uint32(val) : (val))
+
+/* Define common values */
+#define MAC_ADDR_LEN 6
+#define ETHERTYPE_VLAN 0x8100
+#define ETHERTYPE_IPV4 0x0800
+#define ETHERTYPE_IPV6 0x86DD
+#define ETHERTYPE_ARP 0x0806
+#define ETHERTYPE_PTP 0x88F7
+#define ETHERNET_HEADER_LEN 14
+
+/* Functions to swap values */
 uint16_t swap_uint16(uint16_t val);
-/* Function to swap a 32-bit value */
 uint32_t swap_uint32(uint32_t val);
-#endif /* UTILS_H */
+
+#endif // UTILS_H
