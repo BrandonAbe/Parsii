3717ab87c779d87fe0048f2785d4ce39c41d115b
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2026-01-03 15:16:33 -0500

    Updated common definitions, added is_little_endian to function swap to keep scope available throughout project.

--- src/utils.c
+++ src/utils.c
@@ -7,3 +7,9 @@ uint16_t swap_uint16(uint16_t val) {
 uint32_t swap_uint32(uint32_t val) {
     return __builtin_bswap32(val);
 }
+
+int is_little_endian() {
+    volatile uint32_t i = 0x01234567;
+    // return 1 if the first byte (lowest address) is the LSB
+    return (*(volatile uint8_t*)(&i) == 0x67);
+}
