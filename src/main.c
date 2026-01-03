c708ff6a0365b3db7ee0a8f4d8e6968b0f860691
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2026-01-03 15:21:18 -0500

    Cast arg/argv to avoid unused parameter warnings. Updated filepath of PCAP test file.

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 #include "../include/pcap.h" // Include pcap.h for process_pcap_file declaration
 
 int main(int argc, char* argv[]) {
+    (void)argc; //Cast to void to avoid unused parameter warning
+    (void)argv; //Cast to void to avoid unused parameter warning
     printf("PCAP Parser - Main application (under development)\n");
 
     // Define the path to the test PCAP file
-    const char* file_path = "tests/test_samples/Data.pcap";
+    const char* file_path = "./Data.pcap";
 
     // Call the process_pcap_file function
     if (process_pcap_file(file_path) != 0) {
